30b783cc62f962ef516503264e61a7cdc124e9c0
Vikas Hirave <45948809+vikashirave@users.noreply.github.com>
2020-10-19 23:26:26 +0530

    initial

--- database/migrations/2020_10_19_164712_create_emp_table.php
+++ database/migrations/2020_10_19_164712_create_emp_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateEmpTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('emp', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('emp_name');
             $table->string('designation');
             $table->timestamps();
         });
@@ -28,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('emp');
     }
 }
